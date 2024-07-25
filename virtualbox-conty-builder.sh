1a9fe097932975a3f61b998217348d4d8c9f1a90
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-07-25 04:29:27 +0200

    Update virtualbox-conty-builder.sh

--- virtualbox-conty-builder.sh
+++ virtualbox-conty-builder.sh
@@ -70,7 +70,7 @@ fi
 cd .. || exit 1
 
 # EXPORT THE APPDIR TO AN APPIMAGE
-VERSION=$(curl -Ls https://archlinux.org/packages/extra/x86_64/virtualbox/ | grep virtualbox | head -1 | tr ' ' '\n' | grep "^[0-9]" | grep "^pkgver=" | cut -c 8-)
+VERSION=$(curl -Ls https://archlinux.org/packages/extra/x86_64/virtualbox/ | grep virtualbox | head -1 | tr ' ' '\n' | grep "^[0-9]")
 ARCH=x86_64 VERSION="$VERSION" ./appimagetool -s ./"$APP".AppDir
 cd .. && mv ./tmp/*.AppImage ./ || exit 1
 
