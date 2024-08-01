8b8875109247db26b5e9fff9307f130aa0a28672
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-08-02 00:46:59 +0200

    Update create-arch-bootstrap.sh

--- create-arch-bootstrap.sh
+++ create-arch-bootstrap.sh
@@ -367,8 +367,9 @@ fi
 #run_in_chroot locale-gen
 
 # Remove unneeded packages
-run_in_chroot pacman --noconfirm -Rsu base-devel meson mingw-w64-gcc cmake gcc
+run_in_chroot pacman --noconfirm -Rsudd base-devel meson mingw-w64-gcc cmake gcc gtk4
 run_in_chroot pacman --noconfirm -Rdd wine-staging
+run_in_chroot pacman -Qdtq | run_in_chroot pacman --noconfirm -Rsn -
 run_in_chroot pacman --noconfirm -Scc
 
 # Generate a list of installed packages
